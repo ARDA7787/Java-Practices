f979d937bf1584241936a8c555cae952e8c7824c
ARDA7787 <104717701+ARDA7787@users.noreply.github.com>
2025-02-20 21:16:22 -0500

    Add files via upload

---
+++ Assignment2/Lecture02/src/Literals.java
@@ -0,0 +1,16 @@
+
+public class Literals {
+
+	public static void main(String[] args) {
+		System.out.println(1.0 - 0.5);
+		
+		System.out.println(1.0 - 0.1 -0.1 -0.1 -0.1 -0.1);
+		System.out.println("2^3 = " + Math.pow(2, 3));
+		System.out.println("Pi = " + Math.PI);
+		byte b = 127;
+		int intLiteral = 2147483647;
+		long longLiteral = 2147483648L;
+		double doubleLiteral = 1.23456e2d;
+		float myFloat = 1.23456e2F;
+	}
+}
